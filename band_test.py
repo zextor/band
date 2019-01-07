66b3a0679e4e9f6c0cba7c5707c507f78f12f4f6
zextor <zextor@naver.com>
2019-01-07 16:35:54 +0900

    테스트 예제 변경

--- band_test.py
+++ band_test.py
@@ -18,13 +18,12 @@ def cb(text):
 c = band_chatbots.ChatBot()
 c.register_callback(cb)
 
-c.query("새로운 메세지 : 란뽀 : 아파트 뜻")
-c.query("새로운 메세지 : 란뽀 : 더보기 1")
 c.query_weather()
 c.query_keywords()
 c.query_new_book()
 c.query_tv_rating()
 
+# test
 
-
-
+c.query("새로운 메세지 : 란뽀 : 아파트 뜻")
+c.query("새로운 메세지 : 란뽀 : 더보기 1")
