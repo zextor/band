737288913866b5e9afe27e14a6f273cf9545bda7
zextor <zextor@naver.com>
2019-03-13 16:43:05 +0900

    버그 수정

--- band_adapter.py
+++ band_adapter.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
                 # 모듈 교체
                 importlib.reload(band_chatbots)
                 # 인스턴스 새로 생성
-                c = band_chatbots.ChatBot(driver)
+                c = band_chatbots.ChatBot()
                 # 웹드라이버 세팅
                 c.set_driver(driver)
                 # 파일이름 재설정
